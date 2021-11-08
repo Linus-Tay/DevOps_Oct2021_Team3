ae88ee30c80a05f6e56261d8184ec49cd1eb8a3e
Gerald Tan Yuan Xin (ZNX) <gerald.tan@zionext.com>
2021-11-08 10:10:43 +0800

    [DG-55] Updated Main and Game menu and refactor load city

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
+# Imports
 import numpy as np
+import csv
+import copy
 from random import randrange
 from numpy.core.numeric import roll
 
 
-
-
 #Variables
-main_city = []
-
-
-
-
 
 
 #Functions
@@ -32,26 +28,30 @@ def initBuildingPools():
     #Highway: Scores 1 point per connected highway in the same row
 
     #Structured array for buildings pool
-    x = np.array([('BCH',8),('FAC',8),('HSE',8),('SHP',8),('HWY',8)],
+    buildingPools = np.array([('BCH',8),('FAC',8),('HSE',8),('SHP',8),('HWY',8)],
                 dtype=[('Building','U5'),('Copies','<i4')])
-    return x
+    return buildingPools
 
 def viewCity(map):
     for i in map:
-        print(i)
+        print(*i)
+    print()
+
 
 #for loading of new game by reading start.txt which will load the city map
 def loadCity(file):
-    
-    with open(file) as f:
-        for i in f:
-            row = []
-            row.append(i.rstrip())
-            for i in row:
-                main_city.append(i)
-    #display city 
-    viewCity(main_city)
-    
+    mainCity = []
+    with open(file,encoding='utf-8-sig',newline='') as csvfile:
+        spamreader = csv.reader(csvfile)
+        for x in spamreader:
+            col = []
+            for i in x:
+                if i == '*':
+                    i = ' '
+                col.append(i)
+            mainCity.append(col)  
+    playCity=copy.deepcopy(mainCity)
+    return playCity
 
 
 #randomnize 2 building for every turn
@@ -65,64 +65,76 @@ def viewRemainingBuilds(bPool):
     for i in range(len(bPool)):
         print('{}\t\t{}'.format(bPool['Building'][i],bPool['Copies'][i]))
 
-#load options for each turn 
-def gameMenu(bPool):
-        #two random building options
-        b1 = rollBuilding(bPool)
-        b2 = rollBuilding(bPool)
+# Game Menu
+def gameMenu(bPool,playCity,turn):
 
-        #game options
-        game_menu = [[1,'Build a ' + b1],[2,'Build a '+b2],
-        [3,'See remaining buildings'],[4,'See Current Score'],
-        [5,'Save Game'],[0,'Exit to main menu']]
+        while True:
+            # Get Random Building Options
+            b1 = rollBuilding(bPool)
+            b2 = rollBuilding(bPool)
 
-        #displaying game options
-        for i in range(len(game_menu)):
-            print('[{}] {}'.format(game_menu[i][0],game_menu[i][1]))
-            if i == 3:
-                print()
-        game_option= input(str('Your Choice?'))
+            # Game Menu Options
+            game_menu = [[1,'Build a ' + b1],[2,'Build a '+b2],
+            [3,'See remaining buildings'],[4,'See Current Score'],
+            [5,'Save Game'],[0,'Exit to main menu']]
+            
+            # Diplay Turn
+            print('\n-----------------------Turn {}-----------------------\n'.format(turn))
 
-        #build a building
-        if game_option == '1':
-            pass
-        #build a building
-        elif game_option == '2':
-            pass
-        #view remaining building available
-        elif game_option == '3':
-            viewRemainingBuilds(bPool)
-        #view current score
-        elif game_option == '4':
-            pass
-        #save game
-        elif game_option =='5':
-            pass
-        #exit to main menu
-        elif game_option =='0':
-            return
-        else:
-            print("Invalid option, please try again")
+            # Display City
+            viewCity(playCity)
+
+            # Display Game Menu Options
+            for i in range(len(game_menu)):
+                print('[{}] {}'.format(game_menu[i][0],game_menu[i][1]))
+                if i == 3:
+                    print()
+            
+            # Prompt For User's Choice
+            game_option= input(str('\nYour Choice? '))
+
+            # GameOption 1 - Build A Building
+            if game_option == '1':
+                pass
+            # GameOption 2 - Build A Building
+            elif game_option == '2':
+               pass
+            # GameOption 3 - View Remaining Building Available
+            elif game_option == '3':
+                viewRemainingBuilds(bPool)
+            # GameOption 4 - View Current Score
+            elif game_option == '4':
+                pass
+            # GameOption 5 - Save Game
+            elif game_option =='5':
+                pass
+            # GameOption 0 - Exit To Main Menu
+            elif game_option =='0':
+                return
+            else:
+                print("\nInvalid option, please try again")
 
         
 
 
 
-
+# Menu Menu
 while True:
     mainMenu()
     choice = input(str('\nEnter your choice? '))
-    if choice=='1':
-
-        print('\n----------Turn 1----------\n')
-        loadCity('start.txt')
+    # Start New Game
+    if (choice == '1'):    
+        playCity = loadCity('start.csv')
         buildingPools = initBuildingPools()
-        gameMenu(buildingPools)
-    elif choice =='0':
-        break
+        gameMenu(buildingPools,playCity,turn=1)
+    # Load Saved game
+    elif (choice == '2'): 
+        pass
+    # Exit Menu
     elif (choice == '0'):
-        print('\nThank you for playing Simp City!')
+        print('\nThank you for playing Simp City!\n')
         break
+    # Validate for Invalid Input
     else:
         print('\nInvalid option, please try again!')
     
