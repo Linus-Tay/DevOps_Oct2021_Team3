f2ebfb0b897c1f2f04cddb035c6809a616fa0a65
Zhen An Choa <s10198063@connect.np.edu.sg>
2021-11-10 19:56:03 +0800

    [DG-19] Implement beach building. Other buildings implemented as well ref [DG-20], [DG-21], [DG-22], [DG-23]. [DG-27] Option for user to view remaining buildings

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ from numpy.core.numeric import roll
 
 
 #Variables
-
+loc_col = []
+loc_row = []
+inserted_Buildings = []
 
 #Functions
 
@@ -19,18 +21,7 @@ def mainMenu():
         print('[{}] {}'.format(i+1,option_list[i]))
     print('\n[0] Exit')
     
-#initiate building pools with 8 copies of each building for new game
-def initBuildingPools():
-    #Beach: 3 points if it is built on the left or right side of the city, 1 point otherwise.
-    #Factory: 1 point per factory in the city, up to a maximum of 4points for the first 4 factories. All subsequent factories only score 1 point each.
-    #House: if its beside a factory then it scores 1 point only. Other wise it scores 1 point for each adjacent house or shop, and 2 points for each adjacent beach
-    #Shop: scores 1 point per different type of building adjacent to it
-    #Highway: Scores 1 point per connected highway in the same row
 
-    #Structured array for buildings pool
-    buildingPools = np.array([('BCH',8),('FAC',8),('HSE',8),('SHP',8),('HWY',8)],
-                dtype=[('Building','U5'),('Copies','<i4')])
-    return buildingPools
 
 def viewCity(map):
     for i in map:
@@ -53,17 +44,116 @@ def loadCity(file):
     playCity=copy.deepcopy(mainCity)
     return playCity
 
-
-#randomnize 2 building for every turn
-def rollBuilding(bPool):
-
-    b = bPool[randrange(5)]['Building']
-    return b
                 
 def viewRemainingBuilds(bPool):
     print('Buildings\tRemaining\n----------\t----------')
     for i in range(len(bPool)):
         print('{}\t\t{}'.format(bPool['Building'][i],bPool['Copies'][i]))
+
+#----------------------------Building functions----------------------------
+
+#initiate building pools with 8 copies of each building for new game
+def initBuildingPools():
+    #Structured array for buildings pool
+    buildingPools = np.array([('BCH',8),('FAC',8),('HSE',8),('SHP',8),('HWY',8)],
+                dtype=[('Building','U5'),('Copies','<i4')])
+    return buildingPools
+
+#randomnize 2 building for every turn
+def rollBuilding(bPool):
+    b = bPool[randrange(5)]['Building']
+    return b
+
+#Inserting coordinates(col,row) given by user into column and row list
+def insertColRow(arr,letter,number):               
+    for i in arr:
+        if number in i:
+            loc_row.insert(0,arr.index(i))
+            break
+        for x in i:
+            if  x == letter:
+                loc_col.insert(0,i.index(x))
+                break
+
+#sub function(insertBuilding) required to remove coords from the col & row list 
+#in the case where user entered an invalid input for building           
+def removeColRow():
+    loc_col.pop(0)
+    loc_row.pop(0)
+
+#sub function required(insertBuilding) to checks if there is any existing buildings
+def checkExist(arr,nRow,nCol):
+    if arr[nRow[0]][nCol[0]] == " ":
+            return False
+    else:
+        print("You are trying to build on existing building!")
+        return True
+
+#Sub function of (insertBuilding) to perform swapping of 'item' in the city list
+def mSwap(arr,bName,row,col,t):
+    if t == 1:
+        arr[row[0]][col[0]-1] = bName[0][0]
+        arr[row[0]][col[0]] = bName[0][1]
+        arr[row[0]][col[0]+1] = bName[0][2]
+    else:
+        arr[row[0]][col[0]-1] = bName[0][0]
+        arr[row[0]][col[0]] = bName[0][1]
+        arr[row[0]][col[0]+1] = bName[0][2]
+
+#sub function(insertBuilding) required to do the necessary validation checks when user inserts a building
+#True = can build
+#False = cannot build
+def checkCord(arr,nRow,nCol,oRow,oCol):
+    #situation where trying to insert into same row & col
+    if nCol[0] == oCol and nRow[0] == oRow:
+            print("You are trying to build on existing Building!")
+            #remove cordinates from col/row list
+            removeColRow()
+            return False
+    #situation where same column insertion
+    elif nCol[0] == oCol or nRow[0] == oRow:
+        if nRow[0] - 2 == oRow or nRow[0] + 2 == oRow or nCol[0] - 6 == oCol or nCol[0] + 6 == oCol:
+            if checkExist(arr,nRow,nCol) == False:
+                return True
+            else:
+                print("Building already exists at where you try to build")
+                removeColRow()
+                return False
+        else:
+            print("Invalid input, please try again!")
+            removeColRow()
+            return False
+    else:
+        if nRow[0] - 2 == oRow or nRow[0] + 2 == oRow or nCol[0] - 6 == oCol or nCol[0] + 6 == oCol:
+            if checkExist(arr,nRow,nCol) == False:
+                return True
+            else:
+                print("Building already exists at where you try to build")
+                removeColRow()
+                return False
+        else:
+            print("Invalid input, please try again!")
+            removeColRow()
+            return False 
+
+#the main function for inserting a building including all the checks and sub functions
+def insertBuilding(arr,bPool,bName,row,col,t):
+    if t == 1:
+        mSwap(arr,bName,row,col,t)
+        x = np.where(bPool['Building']==bName)
+        index = x[0][0]
+        bPool[index]['Copies']-=1
+        inserted_Buildings.insert(0, bName)
+        #checkCityScore(arr,bName)
+        return (bPool)
+    elif t > 1:
+        if checkCord(arr,row,col,loc_row[1],loc_col[1]) == True:
+            mSwap(arr,bName,row,col,t)
+            inserted_Buildings.insert(0, bName)
+            #checkCityScore(arr,bName)
+    return (bPool)
+
+#---------------------end of Building functions----------------------------
 
 # Game Menu
 def gameMenu(bPool,playCity,turn):
@@ -95,10 +185,30 @@ def gameMenu(bPool,playCity,turn):
 
             # GameOption 1 - Build A Building
             if game_option == '1':
-                pass
+                #get index of building from bPool to retrieve building name
+                x = np.where(bPool['Building'] == b1)
+                currBuild = bPool['Building'][x]
+
+                build_loc = input(str('Build Where? '))
+                # print("col to search {} row to search {}".format(build_loc[0], type(build_loc[1])))
+                insertColRow(playCity,build_loc[0].upper(),build_loc[1])
+                col = loc_col
+                row = loc_row
+                bPool = insertBuilding(playCity,bPool,currBuild,row,col,turn)
+                turn +=1
             # GameOption 2 - Build A Building
             elif game_option == '2':
-               pass
+               #get index of building from bPool to retrieve building name
+                x = np.where(bPool['Building'] == b1)
+                currBuild = bPool['Building'][x]
+
+                build_loc = input(str('Build Where? '))
+                # print("col to search {} row to search {}".format(build_loc[0], type(build_loc[1])))
+                insertColRow(playCity,build_loc[0].upper(),build_loc[1])
+                col = loc_col
+                row = loc_row
+                bPool = insertBuilding(playCity,bPool,currBuild,row,col,turn)
+                turn +=1
             # GameOption 3 - View Remaining Building Available
             elif game_option == '3':
                 viewRemainingBuilds(bPool)
