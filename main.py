744a19a7f5587145a477c05fb35509d7411606d0
Zhen An Choa <s10198063@connect.np.edu.sg>
2021-11-12 12:08:41 +0800

    [DG-19] reference to all building functions, fixed a bug where user were able to build diagonally previously fixed a bug where user's build will be misaligned if building a "FAC" modified code to be tidier

--- main.py
+++ main.py
@@ -65,92 +65,106 @@ def rollBuilding(bPool):
     return b
 
 #Inserting coordinates(col,row) given by user into column and row list
-def insertColRow(arr,letter,number):               
+def insertRowCol(arr,r,c):  
+    if str.upper(c) in arr[0]:
+        loc_col.insert(0,arr[0].index(str.upper(c)))
+        #print("INSERTING COL: {} ".format(loc_col[0]))
     for i in arr:
-        if number in i:
-            loc_row.insert(0,arr.index(i))
-            break
         for x in i:
-            if  x == letter:
-                loc_col.insert(0,i.index(x))
-                break
+            if x == r:
+                loc_row.insert(0,arr.index(i))
+                #print("Inserting row: {} ".format(loc_row[0]))
+    #print("Inserted rows: {}".format(loc_row))
+    #print("Inserted col: {}".format(loc_col))
 
 #sub function(insertBuilding) required to remove coords from the col & row list 
 #in the case where user entered an invalid input for building           
-def removeColRow():
+def removeRowCol():
     loc_col.pop(0)
     loc_row.pop(0)
 
 #sub function required(insertBuilding) to checks if there is any existing buildings
-def checkExist(arr,nRow,nCol):
-    if arr[nRow[0]][nCol[0]] == " ":
+def checkExist(arr,r,c):
+    if arr[r][c] == " ":
             return False
     else:
         print("You are trying to build on existing building!")
         return True
 
 #Sub function of (insertBuilding) to perform swapping of 'item' in the city list
-def mSwap(arr,bName,row,col,t):
-    if t == 1:
-        arr[row[0]][col[0]-1] = bName[0][0]
-        arr[row[0]][col[0]] = bName[0][1]
-        arr[row[0]][col[0]+1] = bName[0][2]
-    else:
-        arr[row[0]][col[0]-1] = bName[0][0]
-        arr[row[0]][col[0]] = bName[0][1]
-        arr[row[0]][col[0]+1] = bName[0][2]
+def mSwap(arr,bName,row,col):
+    arr[row[0]][col[0]-1] = bName[0][0]
+    arr[row[0]][col[0]] = bName[0][1]
+    arr[row[0]][col[0]+1] = bName[0][2]
 
 #sub function(insertBuilding) required to do the necessary validation checks when user inserts a building
-#True = can build
-#False = cannot build
-def checkCord(arr,nRow,nCol,oRow,oCol):
+#True = can build, False = cannot build
+#row/col[1] refers to the previous inserted coords
+#row/col[0] refers to the inserting coords
+def checkCord(arr,row,col):
+    print("rows: {}".format(row))
+    print("cols: {}".format(col))
     #situation where trying to insert into same row & col
-    if nCol[0] == oCol and nRow[0] == oRow:
+    if col[1] == col[0] and row[1] == row[0]:
             print("You are trying to build on existing Building!")
             #remove cordinates from col/row list
-            removeColRow()
+            removeRowCol()
             return False
-    #situation where same column insertion
-    elif nCol[0] == oCol or nRow[0] == oRow:
-        if nRow[0] - 2 == oRow or nRow[0] + 2 == oRow or nCol[0] - 6 == oCol or nCol[0] + 6 == oCol:
-            if checkExist(arr,nRow,nCol) == False:
+    #situation where same column or row insertion
+    elif col[1] == col[0]:
+        print("check #1")
+        if row[1] - 2 == row[0] or row[1] + 2 == row[0]:
+            if checkExist(arr,row[0],col[0]) == False:
                 return True
             else:
-                print("Building already exists at where you try to build")
-                removeColRow()
+                print("Building at row: {} already exists or invalid".format(row[0]))
+                removeRowCol()
                 return False
         else:
-            print("Invalid input, please try again!")
-            removeColRow()
+            print("Row Input: {} is invalid".format(row[0]))
+            removeRowCol()
             return False
-    else:
-        if nRow[0] - 2 == oRow or nRow[0] + 2 == oRow or nCol[0] - 6 == oCol or nCol[0] + 6 == oCol:
-            if checkExist(arr,nRow,nCol) == False:
+    elif row[1] == row[0]:
+        print("check #2")
+        if col[1] - 6 == col[0] or col[1] + 6 == col[0]:
+            if checkExist(arr,row[0],col[0]) == False:
                 return True
             else:
-                print("Building already exists at where you try to build")
-                removeColRow()
-                return False
+                print("Building at col: {} already exists or invalid".format(col[0]))
+                removeRowCol()
+                return False  
         else:
-            print("Invalid input, please try again!")
-            removeColRow()
-            return False 
+            print("Col input: {} is invalid".format(col[0]))
+            removeRowCol()
+            return False
+    elif row[1] - 2 == row[0] or row[1] + 2 == row[0] or col[1] - 6 == col[0] or col[1] + 6 == col[0]:
+            print("check #3")
+            print("You cannot build diagonally, please retry!")
+            removeRowCol()
+            return False
 
 #the main function for inserting a building including all the checks and sub functions
 def insertBuilding(arr,bPool,bName,row,col,t):
+    insertRowCol(arr,row,col)
+    r = loc_row
+    c = loc_col
     if t == 1:
-        mSwap(arr,bName,row,col,t)
+        mSwap(arr,bName,r,c)
         x = np.where(bPool['Building']==bName)
         index = x[0][0]
         bPool[index]['Copies']-=1
         t +=1
-        
+        #inserted_Buildings.insert(0, bName)
+        #checkCityScore(arr,bName)
     elif t > 1:
-        if checkCord(arr,row,col,loc_row[1],loc_col[1]) == True:
-            mSwap(arr,bName,row,col,t)
+        if checkCord(arr,r,c) == True:
+            mSwap(arr,bName,r,c)
             x = np.where(bPool['Building']==bName)
             index = x[0][0]
-            bPool[index]['Copies']-=1 
+            bPool[index]['Copies']-=1
+            t+=1
+            #inserted_Buildings.insert(0, bName)
+            #checkCityScore(arr,bName)
     return (bPool)
 
 #---------------------end of Building functions----------------------------
@@ -190,11 +204,8 @@ def gameMenu(bPool,playCity,turn):
                 currBuild = bPool['Building'][x]
 
                 build_loc = input(str('Build Where? '))
-                # print("col to search {} row to search {}".format(build_loc[0], type(build_loc[1])))
-                insertColRow(playCity,build_loc[0].upper(),build_loc[1])
-                col = loc_col
-                row = loc_row
-                bPool = insertBuilding(playCity,bPool,currBuild,row,col,turn)
+                print("col to search {} row to search {}".format(build_loc[0], build_loc[1]))
+                bPool = insertBuilding(playCity,bPool,currBuild,build_loc[1],build_loc[0],turn)
                 turn +=1
             # GameOption 2 - Build A Building
             elif game_option == '2':
@@ -203,11 +214,8 @@ def gameMenu(bPool,playCity,turn):
                 currBuild = bPool['Building'][x]
 
                 build_loc = input(str('Build Where? '))
-                # print("col to search {} row to search {}".format(build_loc[0], type(build_loc[1])))
-                insertColRow(playCity,build_loc[0].upper(),build_loc[1])
-                col = loc_col
-                row = loc_row
-                bPool = insertBuilding(playCity,bPool,currBuild,row,col,turn)
+                print("col to search {} row to search {}".format(build_loc[0], build_loc[1]))
+                bPool = insertBuilding(playCity,bPool,currBuild,build_loc[1],build_loc[0],turn)
                 turn +=1
             # GameOption 3 - View Remaining Building Available
             elif game_option == '3':
