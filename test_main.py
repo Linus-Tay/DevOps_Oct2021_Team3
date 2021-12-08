371ed8983db9b55be3d1f1bcc54b1a8f8bf52d89
Gerald Tan Yuan Xin (ZNX) <gerald.tan@zionext.com>
2021-12-08 17:36:27 +0800

    [DG-18] Refactor MainMenu Code and UnitTest

--- test_main.py
+++ test_main.py
@@ -1,11 +1,53 @@
 import main
 import unittest
+from testBase import get_display_output, set_keyboard_input
 
 class test_MainMenu_function(unittest.TestCase):
-    def test_MainMenu(self):
-        print("\nTest MainMenu\nStart")
-        self.assertIsNone(main.mainMenu())
-        print("\nEnd")
+    def test_MainMenu_Opt1(self):
+        set_keyboard_input(['1'])
+        main.mainMenu()
+        output = get_display_output()
+        assert output == ["\nWelcome, mayor of Simp City!",
+                            "----------------------------",
+                            "[1] Start new game",
+                            "[2] Load saved game",
+                            "\n[0] Exit",
+                            "\nEnter your choice? ",
+                            '\noption 1']
+    def test_MainMenu_Opt2(self):
+        set_keyboard_input(['2'])
+        main.mainMenu()
+        output = get_display_output()
+        assert output == ["\nWelcome, mayor of Simp City!",
+                            "----------------------------",
+                            "[1] Start new game",
+                            "[2] Load saved game",
+                            "\n[0] Exit",
+                            "\nEnter your choice? ",
+                            '\noption 2']
+    def test_MainMenu_Opt0(self):
+        set_keyboard_input(['0'])
+        main.mainMenu()
+        output = get_display_output()
+        assert output == ["\nWelcome, mayor of Simp City!",
+                            "----------------------------",
+                            "[1] Start new game",
+                            "[2] Load saved game",
+                            "\n[0] Exit",
+                            "\nEnter your choice? ",
+                            "\nThank you for playing Simp City!"]
+    def test_MainMenu_InvalidOpt(self):
+        set_keyboard_input(['123'])
+        main.mainMenu()
+        output = get_display_output()
+        assert output == ["\nWelcome, mayor of Simp City!",
+                            "----------------------------",
+                            "[1] Start new game",
+                            "[2] Load saved game",
+                            "\n[0] Exit",
+                            "\nEnter your choice? ",
+                            '\nInvalid option, please try again!']
 
-if __name__ == "__main__":
-    unittest.main()
+
+# if __name__ == "__main__":
+#     unittest.main()
